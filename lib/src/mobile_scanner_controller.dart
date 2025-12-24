0252b405142d18b7150f9ed0c8b12a20a003104c
adriano-evocorp <adriano@evocorp.com.br>
2025-12-24 09:01:24 -0300

    fix: force device orientation to portraitUp to avoid rotation issues

--- lib/src/mobile_scanner_controller.dart
+++ lib/src/mobile_scanner_controller.dart
@@ -467,7 +467,8 @@ class MobileScannerController extends ValueNotifier<MobileScannerState> {
           isStarting: false,
           isRunning: true,
           size: viewAttributes.size,
-          deviceOrientation: viewAttributes.initialDeviceOrientation,
+          // deviceOrientation: viewAttributes.initialDeviceOrientation,
+          deviceOrientation: DeviceOrientation.portraitUp,
           // Provide the current torch state.
           // Updates are provided by the `torchStateStream`.
           torchState: viewAttributes.currentTorchMode,
